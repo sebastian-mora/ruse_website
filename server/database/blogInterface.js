dbe6143f144cf7679309a2a1bdad44ac3a913de3
Sebastian <sebmora1055@gmail.com>
2020-05-23 23:01:26 -0700

    refactored blog database and added isPosted into database

---
+++ server/database/blogInterface.js
@@ -0,0 +1,59 @@
+var db  = require('../database/database')
+
+function getAllBlogs(){
+  return new Promise(function(resolve, reject) {
+    // The Promise constructor should catch any errors thrown on
+    // this tick. Alternately, try/catch and reject(err) on catch.
+
+
+    var query_str = 'SELECT * FROM  blogs WHERE isPosted=1'
+
+    db.query(query_str, function (err, rows, fields) {
+        // Call reject on error states,
+        // call resolve with results
+        if (err) {
+            return reject(err);
+        }
+        resolve(rows);
+    });
+  }); 
+}
+
+function getBlogByID(id){
+  return new Promise(function(resolve, reject) {
+    // The Promise constructor should catch any errors thrown on
+    // this tick. Alternately, try/catch and reject(err) on catch.
+
+
+    var query_str = `SELECT * FROM  blogs WHERE id=${id} AND isPosted=1`
+
+    db.query(query_str, function (err, rows, fields) {
+        // Call reject on error states,
+        // call resolve with results
+        if (err) {
+            return reject(err);
+        }
+        resolve(rows);
+    });
+  }); 
+}
+
+
+function addBlog(blog){
+  return new Promise((resolve, reject) => {
+    var query_str = `INSERT INTO blogs (title, date_created, post) VALUES('${blog.title}', '1/1/1', '${blog.post}');`
+
+    db.query(query_str, (err)=>{
+      if(err)
+        return reject(err);
+      
+      resolve();
+    })
+  })
+}
+
+module.exports = {
+  addBlog,
+  getAllBlogs,
+  getBlogByID
+}
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,68 +1,10 @@
 var express = require('express')
 var router = express.Router()
-var db  = require('../database/database')
+
 const verifyToken = require('./middleware/Auth')
+const {getAllBlogs, getBlogByID, addBlog}  = require('../database/blogInterface')
 
 router.use(express.json())
-
-const sanitize_hash = (input) =>{
-  return input.replace('\b[A-Fa-f0-9]{64}\b', '')
-}
-
-
-
-function getAllBlogs(){
-  return new Promise(function(resolve, reject) {
-    // The Promise constructor should catch any errors thrown on
-    // this tick. Alternately, try/catch and reject(err) on catch.
-
-
-    var query_str = 'SELECT * FROM  blogs'
-
-    db.query(query_str, function (err, rows, fields) {
-        // Call reject on error states,
-        // call resolve with results
-        if (err) {
-            return reject(err);
-        }
-        resolve(rows);
-    });
-  }); 
-}
-
-function getBlogByID(id){
-  return new Promise(function(resolve, reject) {
-    // The Promise constructor should catch any errors thrown on
-    // this tick. Alternately, try/catch and reject(err) on catch.
-
-
-    var query_str = `SELECT * FROM  blogs WHERE id=${id}`
-
-    db.query(query_str, function (err, rows, fields) {
-        // Call reject on error states,
-        // call resolve with results
-        if (err) {
-            return reject(err);
-        }
-        resolve(rows);
-    });
-  }); 
-}
-
-
-function addBlog(blog){
-  return new Promise((resolve, reject) => {
-    var query_str = `INSERT INTO blogs (title, date_created, post) VALUES('${blog.title}', '1/1/1', '${blog.post}');`
-
-    db.query(query_str, (err)=>{
-      if(err)
-        return reject(err);
-      
-      resolve();
-    })
-  })
-}
-
 
 
 // create a GET route
