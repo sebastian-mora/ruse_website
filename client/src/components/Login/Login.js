b8961cba196afe26fadd3d78d4b20f8afde1261e
Sebastian <sebmora1055@gmail.com>
2020-06-13 19:34:24 -0700

    Fixed login behavior

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,36 +1,44 @@
 import React, {useState} from 'react';
 import {useHistory} from 'react-router-dom';
 
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import {loginUser} from '../../redux/actions/authActions'
 
 const Login = ({dispatch}) => {
-
+  const isAuthd = useSelector(state => state.user.isAuthd)
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [valid, setValid] = useState(false);
   const history = useHistory();
 
   function validateForm() {
     if (username.length > 0 && password.length > 0){
-      setValid(true);
+      return true
     }
+  }
+
+  function redirectToAdmin(){
+    history.push('/admin')
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    if(valid){  
+    if(validateForm()){  
       dispatch(loginUser(username, password))
-      history.push('/admin')
+      redirectToAdmin()
     }
   }
 
-  return (
+  //Check if user is already logged in 
 
+  if(isAuthd){
+    redirectToAdmin()
+  }
+
+  return (
     <div className="Login">
       <form onSubmit={handleSubmit} onChange={validateForm}>
         <input type="username" value={username} onChange={e => setUsername(e.target.value)}></input>
-        <input type="pasword" value={password} onChange={e => setPassword(e.target.value)}></input>
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
         <input type="submit" value="Submit" />
       </form>
     </div>
