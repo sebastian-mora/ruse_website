1bf8900101f3814a5ff3ec050b808b2b6d48d29c
Sebastian <sebmora1055@gmail.com>
2020-06-13 20:07:12 -0700

    Convered to prepared statements

--- server/database/blogInterface.js
+++ server/database/blogInterface.js
@@ -24,15 +24,7 @@ function getAllBlogs(isAdmin=false){
 
 function getBlogByID(id){
   return new Promise(function(resolve, reject) {
-    // The Promise constructor should catch any errors thrown on
-    // this tick. Alternately, try/catch and reject(err) on catch.
-
-
-    var query_str = `SELECT title,id,post,date,views FROM  blogs WHERE id=${id} AND isPosted=true`
-
-    pool.query(query_str, function (err, rows, fields) {
-        // Call reject on error states,
-        // call resolve with results
+    pool.query('SELECT title,id,post,date,views FROM  blogs WHERE id=? AND isPosted=true', id, function (err, rows, fields) {
         if (err) {
             return reject(err);
         }
@@ -43,35 +35,24 @@ function getBlogByID(id){
 
 
 function addBlog(blog){
-  const {title, date, post, isPosted} = blog;
-
   console.log(blog);
-  
-  return new Promise((resolve, reject) => {
-    var query_str = `INSERT INTO blogs (title, date, post, isPosted) VALUES('${title}', '${date}', '${post}', '${+ isPosted}');`
-
-    pool.query(query_str, (err)=>{
-      if(err)
-        return reject(err);
-  
-      resolve();
-    })
-  })
+  return new Promise((resolve, reject) =>
+    pool.query('INSERT INTO blogs SET ?', blog, (err) =>{
+      if(err){
+        return reject(err)
+      }
+      resolve()
+    }))
 }
+
 
 
 
 function updateBlog(blog)
 {
-  const {id, title, date, post, isPosted} = blog;
-
-  console.log(id, title, date, post, isPosted);
-  
-
   return new Promise((resolve, reject) => {
-    var query_str = `UPDATE blogs SET title='${title}', post='${post}', date= '${date}', isPosted=${isPosted} WHERE id=${id};`
 
-    pool.query(query_str, (err)=>{
+    pool.query('UPDATE blogs SET ? WHERE id=?', [{title,date,post,isPosted}=blog, blog.id] , (err)=>{
       if(err)
         return reject(err);
   
@@ -84,15 +65,11 @@ function updateBlog(blog)
 function deleteBlog(id)
 {
   return new Promise((resolve, reject) =>{ 
-    var query_str = `DELETE FROM blogs WHERE id=${id};`
-
-    pool.query(query_str, (err)=>{
+    pool.query('DELETE FROM blogs WHERE id=?', id, (err)=>{
       if(err)
         return reject(err);
-  
       resolve();
     })
-
   })
 }
 
