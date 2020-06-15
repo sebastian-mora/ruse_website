87c1091df7bee2c5e1a821137868668d8ad6b6d7
Sebastian <sebmora1055@gmail.com>
2020-06-14 20:37:54 -0700

    added basic styling to about

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import style from './About.module.css'
 
 const About = () => {
 
   return (
-    <div>
-    <p>
-    This website is dedicated to whatever content I feel is interesting or relevant.
-    If you wish to contact me email me at <a href="mailto:ruse@ruse.tech">ruse@ruse.tech</a>
-    </p>
+    <div className={style.center}>
+      <p className={style.boxed}>
+      This website is dedicated to whatever content I feel is interesting or relevant.
+      If you wish to contact me email me at <a href="mailto:ruse@ruse.tech">ruse@ruse.tech</a>
+      <br></br>
+      <a href="https://github.com/seb1055">GIT<img src="public/imgs/github.png" alt=""/></a>
+      </p>
+     
     </div>
   )
 }
