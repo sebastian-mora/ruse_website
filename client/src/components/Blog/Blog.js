8054b7adccf79e5242fb93cc9892dc769e19b513
Sebastian <sebmora1055@gmail.com>
2020-06-17 22:02:07 -0700

    fixing breaking merge smh

--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -11,6 +11,7 @@ const Blog = () => {
   // store the blogs from the api in blogs
   const [blogs, setBlogs] = useState({});
   const [loadErr, setErr] = useState(false);
+  const NEW_POST_THRESHOLD = 30;
 
  
   useEffect(() => {
@@ -26,11 +27,26 @@ const Blog = () => {
 
   const processCategories = (blogs) => {
 
-    let sortedBlogs = {}
+    // The new category is initalized bc it is a constant catagory.
+    // Other keys added dynamicly 
+
+    let sortedBlogs = {
+      "new":[]
+    }
+    var current_date = new Date()
     
     // Generate an organized Blogs Object
-    blogs.map(({category, title, id}) => {
+    blogs.map(({category, title, id, date}) => {
 
+      var blog_date = new Date(date)
+      var post_delta = (current_date - blog_date)/(1000*60*60*24.0)
+
+      // If the blog is less than 30 days add it to the new category
+      if(post_delta <= NEW_POST_THRESHOLD)
+      {
+        sortedBlogs.new.push({id,title})
+      }
+  
       // If category does not exist create it and add it
       if (!(category in sortedBlogs))
       {
@@ -38,24 +54,15 @@ const Blog = () => {
           [category]:[{title,id}]
         }
       }
+
       // else the catagoie does exist. Add it to the category 
       else 
       {
         sortedBlogs[category].push({id,title})
       }
     })
-    
+ 
     return sortedBlogs
-  }
-
-
-  function lessThan30Days(blog){
-
-    var blog_date = new Date(blog.date)
-    var current_date = new Date()
-    var delta = (current_date - blog_date)/(1000*60*60*24.0)
-
-    return delta <= 30
   }
 
   return (
@@ -66,7 +73,6 @@ const Blog = () => {
   
       <ul className={style.root}>
 
-
       {/* 
       
         Loop through the sorted blog object keys.
@@ -76,20 +82,12 @@ const Blog = () => {
       */}
       {Object.keys(blogs).map((keyName, i) => (
         <React.Fragment key={keyName}>
-        <li>{keyName}</li>
-        <ul className={style.sub}> 
-          {blogs[keyName].map((blog) => {
-            return <Link key={blog.id} to={`blog/${blog.id}`}><li key={i+2}>{blog.title}</li></Link>
-          } )}
-
-        <li>Last 30 Days: </li>
-        <ul className={style.sub}>
-          {blogs.filter(lessThan30Days).map(({id,title, date}) => {
-            return <Link key={id} to={`blog/${id}`}><li>{title} - {date}</li></Link>
-          })}
-        </ul>
-
-        </ul>
+          <li>{keyName}</li>
+          <ul className={style.sub}> 
+            {blogs[keyName].map((blog) => {
+              return <Link key={blog.id + i} to={`blog/${blog.id}`}><li>{blog.title}</li></Link>
+            })}
+          </ul>
         </React.Fragment>
       ))}
       
