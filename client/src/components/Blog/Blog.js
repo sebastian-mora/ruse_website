d87f6124b9f895c1c050978f81a5160b6794776b
Sebastian <sebmora1055@gmail.com>
2020-06-17 20:48:35 -0700

    Added recent area for blogs

--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -20,19 +20,38 @@ const Blog = () => {
       });
   }, []);
 
-    
+
+  function lessThan30Days(blog){
+
+    var blog_date = new Date(blog.date)
+    var current_date = new Date()
+    var delta = (current_date - blog_date)/(1000*60*60*24.0)
+
+    return delta <= 30
+  }
+
+  console.log(blogs.filter(lessThan30Days));
+  
+
   return (
     <div className={style.center}>
 
       <h1>Blogs:</h1>
 
-
+  
       <ul className={style.root}>
+
+        <li>Last 30 Days: </li>
+        <ul className={style.sub}>
+          {blogs.filter(lessThan30Days).map(({id,title, date}) => {
+            return <Link key={id} to={`blog/${id}`}><li>{title} - {date}</li></Link>
+          })}
+        </ul>
 
         <li>General Blgos</li>
           <ul className={style.sub}>
-            {blogs.map(({id,title}) =>{
-              return <Link key={id} to={`blog/${id}`}><li>{title}</li></Link>
+            {blogs.map(({id,title, date}) =>{
+              return <Link key={id} to={`blog/${id}`}><li>{title} - {date}</li></Link>
             })}
         </ul>
 
