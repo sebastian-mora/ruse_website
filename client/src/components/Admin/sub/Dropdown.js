0d621700d670f216ad60aa8e78339f2a212d9287
Sebastian <sebmora1055@gmail.com>
2020-07-31 10:48:03 -0700

    fixed bug in drop down

--- client/src/components/Admin/sub/Dropdown.js
+++ client/src/components/Admin/sub/Dropdown.js
@@ -7,6 +7,7 @@ const Dropdown = (props) => {
   return (
     <div>
       <select name={props.name} onChange={e => props.onChange(e)}>
+        <option key={-1} value=""></option>
         {props.options.map(({title, id}) =>{
           return <option  key={id} value={id}>{title}</option>
         })}
