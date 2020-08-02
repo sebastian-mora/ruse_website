a2b9242d904f99681b33900bdd054d8d654bcecd
Sebastian <sebmora1055@gmail.com>
2020-08-01 21:03:45 -0700

    added env

--- server/database/database.js
+++ server/database/database.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 const util = require('util')
 
 var pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database:'website'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database:process.env.DB_NAME
 });
 
 
