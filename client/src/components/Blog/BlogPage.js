b6275dc3d00a8096e6c940d79f785b992f64c558
Sebastian <sebmora1055@gmail.com>
2020-12-12 20:35:09 +0000

    updated client to render .md

--- client/src/components/Blog/BlogPage.js
+++ client/src/components/Blog/BlogPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown'
 
 import style from './BlogPage.module.css'
 
@@ -27,9 +28,7 @@ const BlogPage = (props) => {
 
       <div className={style.date}>{blog.date}</div>
 
-      <div className={style.post}   dangerouslySetInnerHTML={{__html: blog.post}}>
-
-      </div>
+      <ReactMarkdown className= {style.post} >{blog.post}</ReactMarkdown>
 
     </div>
   )
