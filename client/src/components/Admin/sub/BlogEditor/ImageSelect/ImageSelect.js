61e79bb8d6cd6d18009697219a2d2b33e24d5d20
Sebastian <sebmora1055@gmail.com>
2020-12-19 20:54:28 +0000

    fixed sync issues with image upload

--- client/src/components/Admin/sub/BlogEditor/ImageSelect/ImageSelect.js
+++ client/src/components/Admin/sub/BlogEditor/ImageSelect/ImageSelect.js
@@ -1,100 +1,81 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useEffect, useCallback} from 'react';
 import { connect, useDispatch } from 'react-redux';
-import {} from '../../../../../redux/actions/blogActions';
 
 import {useDropzone} from 'react-dropzone'
 
-import {getImages, uploadImage} from '../../../../../api/adminApi'
+import { uploadImage} from '../../../../../api/adminApi'
+import {fetchBlogImages, saveBlogImage} from '../../../../../redux/actions/editorActions'
 import style from './ImageSelect.module.css'
 import { UPDATE_EDITOR_BLOG } from '../../../../../redux/actions/types';
 
 
 const ImageSelect = (props) => {
 
-  const [images, setImages] = useState([])
+
   const dispatch = useDispatch()
 
   useEffect( () => {
-    setImages(loadImages(props.blog_id))
-  }, [])
+    if(props.images.length == 0 || props.newUploadedImage){
+      dispatch(fetchBlogImages(props.blog_id))
+    }
+  }, [props.newUploadedImage])
 
-  function loadImages(blog_id){
 
-    getImages(blog_id).then((data) =>{
-      setImages(data);
-    })
-    .catch((err) => {
-      return;
-    })
-    return []
-  }
-
+  // on click insert the .md line into the blog
   function imgClick(e) {
-
     const html_img = `![alt text](${e.target.src})`
     dispatch({type: UPDATE_EDITOR_BLOG, payload: {...props.blog, post: props.blog.post + '\n' +  html_img }})
   }
 
   const onDrop = useCallback(acceptedFiles => {
-    acceptedFiles.forEach((file) => {
-        uploadImage(props.blog_id, file).then((res) =>{
-            console.log(res);
-          }).catch((err) => {
-            console.log(err);
-          })
-    }, [])
+      acceptedFiles.forEach((file) => {
+        dispatch(saveBlogImage(props.blog_id, file))
+      }, [])
+
   })
 
   const {getRootProps, getInputProps} = useDropzone({onDrop})
 
 
   return (
-    
-    <div>
-      {/* Show images */}
+      
       <div>
-        <p>YOOOO</p>
-        {/* {render_images()} */}
+        {/* Show images */}
+        <div>
+          {
+            props.images.map(image => (
+              <div key={image.url}  className={style.thumbnail} >
+              <img onClick={imgClick} alt={image.filename} src={image.url}/>
+              <p>{image.filename}</p>
+            </div> 
+            ))
+          }
+  
+        </div>
+        
 
-        {
-          images.map(image => (
-            <div key={image.url}  className={style.thumbnail} >
-            <img onClick={imgClick} alt={image.filename} src={image.url}/>
-            <p>{image.filename}</p>
-          </div> 
-          ))
-        }
- 
+        {/* Image Upload Area */}
+        <div className={style.Upload} {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Click or drop images here</p>
+        </div>
+        
       </div>
-      
-
-      {/* Image Upload Area */}
-      <div className={style.Upload} {...getRootProps()}>
-        <input {...getInputProps()} />
-        <p>Click or drop images here</p>
-      </div>
-      
-    </div>
-  )
+    )
 }
 
 const mapToProps= (state) =>{
 
-  let {post, title, date ,isPosted, id, category} = state.editor.editorBlog
-  let {isNewPost, categories, preview} = state.editor;
+  let {post, id } = state.editor.editorBlog
+  let {images, newUploadedImage} = state.editor;
 
   return {
     blog:{
       post,
-      title,
-      date,
-      isPosted,
-      id,
-      category
+      id
     },
-    isNewPost,
-    categories,
-    preview
+    images,
+    newUploadedImage
   }
 }
 
