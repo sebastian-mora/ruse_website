38ebc4b7e815d687fdf077bf037bf337fb0ddd4d
Sebastian <sebmora1055@gmail.com>
2020-12-24 00:29:03 -0800

    Soft crash when clicking "New Blog" (#80)

--- client/src/redux/reducers/editorReducer.js
+++ client/src/redux/reducers/editorReducer.js
@@ -58,7 +58,8 @@ export default function(state=intialState, action){
       return {
         ...state,
         editorShow: true,
-        isNewPost: true
+        isNewPost: true,
+        loaded: true
       }
     
     case EDITOR_FETCH_BLOG_SUCCESS:
@@ -70,7 +71,8 @@ export default function(state=intialState, action){
     case EDITOR_FETCH_BLOG_FAILURE:
       return {
         ...state,
-        error: action.payload
+        error: action.payload,
+        loaded: true
       }
 
     case EDITOR_FETCH_BLOG_REQUEST:
