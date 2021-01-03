49320b44ae92bac1a4d07f6525a2d74e83adf63d
Sebastian <sebmora1055@gmail.com>
2021-01-03 00:12:52 +0000

    better grammer

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -10,9 +10,9 @@ const About = () => {
     <h1><code>typeof(RU$E)</code></h1>
       <img src={ruse_dark} className={style.ruseImg} alt="Ruse Logo"></img>
       <p className={style.boxedAbout}>
-        I am an infosec person. I am a Pentester with focus on Cloud/WebApps and an intrest in privacy.
+        I am an infosec person and a Pentester with focus on Cloud/WebApps and an intrest in privacy.
         This website is dedicated to whatever content I feel is interesting or relevant.
-        If you wish to contact my email is <a href=""> seb@ruse.tech</a> (<a href="https://keys.openpgp.org/vks/v1/by-fingerprint/61594A228EE2FF3B7811A5C0423803123B071223">PGP Available</a>).
+        If you wish to contact my email is <a href=""> seb@ruse.tech </a> (<a href="https://keys.openpgp.org/vks/v1/by-fingerprint/61594A228EE2FF3B7811A5C0423803123B071223">PGP Available</a>).
 
         <br></br>
         Links:
@@ -21,11 +21,7 @@ const About = () => {
           <li><a href="https://twitter.com/seb1055"  rel="noopener noreferrer" target="_blank">Twitter</a></li>
         </ul>
       </p>
-
- 
     </div>
-
-    
   )
 }
 
