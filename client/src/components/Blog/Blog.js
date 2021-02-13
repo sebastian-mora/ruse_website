3f055c38a40deec032063e5c867e686557f2cba2
Sebastian <sebmora1055@gmail.com>
2021-02-13 23:07:18 +0000

    removing deps

--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -4,23 +4,28 @@ import {getAllBlogs} from '../../api/blogsApi';
 import style from './Blog.module.css'
 
 
-
-
 const Blog = () => {
 
   // store the blogs from the api in blogs
   const [blogs, setBlogs] = useState([]);
 
- 
+
   useEffect(() => {
     getAllBlogs()
       .then(res => {
+        res.data.sort((a, b) => {
+          return  convertStringToDate(b.metadata.datePosted) - convertStringToDate(a.metadata.datePosted) 
+        })
         setBlogs(res.data)
       })
   }, []);
 
 
-  
+  // DATE FORMAT IS MM/DD/YYYY
+  const convertStringToDate = (dateString) => {
+    const date  = dateString.split('/')
+    return new Date(date[2], date[0], date[1])
+  }
 
   return (
     <div className={style.center}>
@@ -28,9 +33,17 @@ const Blog = () => {
       <h1 className={style.pageTitle}>Blogs:</h1>
   
       <ul className={style.root}>
-        {blogs.map(blog => {
-          return <Link key={blog} to={`blogs/${blog}`}><li className={style.post}>{blog}</li></Link>
+
+        {blogs.map((blog, index) => {
+          return( 
+                <div className={style.linkContainer}> 
+                  <Link key={index} to={`blogs/${blog.path}`}> <li className={style.link}>{blog.metadata.title}</li></Link>
+                  <p className={style.postDate}>Date: {blog.metadata.datePosted}</p>
+                  <p>Tags: {blog.metadata.tags.join(', ')}</p>
+                </div>
+          ) 
         })}
+        
       </ul>
     </div>
   )
