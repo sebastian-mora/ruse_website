2142c4d0cfa590cac944b3d63f4b448482f8afb0
Sebastian <sebmora1055@gmail.com>
2021-05-04 20:16:12 -0700

    better card layout

--- src/components/Blog/Card.js
+++ src/components/Blog/Card.js
@@ -10,19 +10,22 @@ const Card = ({blog}) => {
 
     <Link className={style.link} to={`blogs/${id}`}> 
       <div className={style.card}>
-        <h1 className={style.title}>{title}</h1>
-        <p className={style.postDate}>Date: {datePosted}</p>
 
-        <div className={style.tagGroup}>
-          {tags.map((tag, _) => {
-            return( 
-                <p className={style.tag}>{tag}</p>
-            ) 
-          })}
+        <img className={style.blogImage} src={`https://via.placeholder.com/200x200?text=${title}"`}></img>
+        <div className={style.text}>
+          <h1 className={style.title}>{title}</h1>
+          <p className={style.postDate}>Date: {datePosted}</p>
+
+          <div className={style.tagGroup}>
+            {tags.map((tag, _) => {
+              return( 
+                  <p className={style.tag}>{tag}</p>
+              ) 
+            })}
+          </div>
+          <p className={style.description}> {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, "}</p>
         </div>
-
-        <img className={style.blogImage} src={`https://via.placeholder.com/468x60?text=${title}"`}></img>
-        <p className={style.description}> {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}</p>
+        
       </div>
     </Link>
 
