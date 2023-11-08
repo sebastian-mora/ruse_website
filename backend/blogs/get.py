93b6ed712131c8f8f19d05edfd6d61bc62f93a08
seb <seb@ruse.tech>
2023-11-08 16:51:18 -0500

    fmt

--- backend/blogs/get.py
+++ backend/blogs/get.py
@@ -2,52 +2,43 @@ import json
 import boto3
 import os
 
-headers = {
-    "Content-Type": "application/json",
-    'Access-Control-Allow-Origin': '*'
-}
-
-bucket = os.environ['BUCKET']
-blog_index_file = os.environ['BLOGINDEX']
+headers = {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"}
 
 
 def get_blog(blog_id):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('ruse-tech-blogs')
-    response = table.get_item(Key={
-        "id": blog_id
-    })
-    return response.get('Item')
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("ruse-tech-blogs")
+    response = table.get_item(Key={"id": blog_id})
+    return response.get("Item")
 
 
 def get(event, context):
     # Get the blog title from request
     try:
-        blog_id = event['pathParameters']['title']
-        
+        blog_id = event["pathParameters"]["title"]
+
     except Exception:
         return {
             "statusCode": 403,
-            "body": json.dumps({"message":"Blog path not specified."}),
-            "headers":headers,
-            'isBase64Encoded': False
+            "body": json.dumps({"message": "Blog path not specified."}),
+            "headers": headers,
+            "isBase64Encoded": False,
         }
 
     blog_data = get_blog(blog_id)
-    
+
     if blog_data:
         return {
             "statusCode": 200,
             "body": json.dumps(blog_data),
-            "headers":headers,
-            'isBase64Encoded': False
-        }
-    
-    else:
-         return {
-            "statusCode": 404,
-            "body": json.dumps({"message":"Blog not found."}),
-            "headers":headers,
-            'isBase64Encoded': False
+            "headers": headers,
+            "isBase64Encoded": False,
         }
 
+    else:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"message": "Blog not found."}),
+            "headers": headers,
+            "isBase64Encoded": False,
+        }
--- backend/blogs/list.py
+++ backend/blogs/list.py
@@ -1,43 +1,39 @@
 import json
-import os
-import boto3 
+import boto3
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource("dynamodb")
 
-headers = {
-    "Content-Type": "application/json",
-    'Access-Control-Allow-Origin': '*'
-}
+headers = {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"}
+
 
 def get_blogs():
-    table = dynamodb.Table('ruse-tech-blogs')
+    table = dynamodb.Table("ruse-tech-blogs")
     response = table.scan()
-    data = response['Items']
+    data = response["Items"]
     # Drop the blog data in the response
     for b in data:
-        del b['blog']
+        del b["blog"]
 
     return data
 
 
 def list(event, context):
-   
     try:
         data = get_blogs()
         return {
             "statusCode": 200,
             "body": json.dumps(data),
-            "headers":headers,
-            'isBase64Encoded': False
+            "headers": headers,
+            "isBase64Encoded": False,
         }
-        
+
     except Exception as e:
         return {
             "statusCode": 500,
-            "body": json.dumps({"message":"Failed to load blogs."}),
+            "body": json.dumps({"message": "Failed to load blogs."}),
             "headers": {
                 "Content-Type": "application/json",
-                'Access-Control-Allow-Origin': '*'
+                "Access-Control-Allow-Origin": "*",
             },
-            'isBase64Encoded': False
+            "isBase64Encoded": False,
         }
