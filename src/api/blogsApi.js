5ff5f5075c8a7d8a784a4ddce8ceb3610b777d85
seb <seb@ruse.tech>
2023-11-09 16:49:32 +0000

    remove old apis

--- src/api/blogsApi.js
+++ src/api/blogsApi.js
@@ -1,34 +1,15 @@
 import axios from 'axios';
 
-import {API_ENDPOINT} from '../config'
+import { API_ENDPOINT } from '../config'
 
 export const api = axios.create()
 
-export function getBlogBySlug(slug){
+export function getBlogBySlug(slug) {
   return api.get(`${API_ENDPOINT}/blogs/${slug}`)
 }
 
-export function getAllBlogs(){
+export function getAllBlogs() {
   return api.get(`${API_ENDPOINT}/blogs`)
-}
-
-export function postBlog(formData, token){
-  const config = {
-    headers: {
-        'Content-Type': 'multipart/form-data',
-        'Authorization': `${token}`
-    }
-  }
-  return api.post(`${API_ENDPOINT}/blogs/add`, formData, config)
-}
-
-export function deleteBlog(id, token){
-  const config = {
-    headers: {
-        'Authorization': `${token}`
-    }
-  }
-  return api.post(`${API_ENDPOINT}/blogs/delete`, {id}, config)
 }
 
 
