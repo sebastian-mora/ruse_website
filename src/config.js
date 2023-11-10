ca3496ab77ae58662ae89c7d390bcc32c149541c
seb <seb@ruse.tech>
2023-11-10 02:05:51 +0000

    build env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export const API_ENDPOINT = process.env.ENV === 'prod'
+const BUILD_ENV = process.env.REACT_APP_BUILD_ENV || "dev"
+
+export const API_ENDPOINT = BUILD_ENV === 'prod'
     ? 'https://api.ruse.tech'  // Use the production endpoint
     : 'https://api.ruse.tech/dev'; // Use the development endpoint or any other desired value for 'dev'
