8605b5a9c94fb1af8eef0d9db0ce6860e0742eba
seb <seb@ruse.tech>
2023-11-12 23:25:38 -0500

    fix logic

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-const BUILD_ENV = process.env.REACT_APP_BUILD_ENV || "dev"
+const BUILD_ENV = process.env.REACT_APP_BUILD_ENV;
 
-export const API_ENDPOINT = BUILD_ENV === 'prod'
-    ? 'https://api.ruse.tech'  // Use the production endpoint
-    : 'https://api.ruse.tech/dev'; // Use the development endpoint or any other desired value for 'dev'
+export const API_ENDPOINT =
+  BUILD_ENV === "dev"
+    ? "https://api.ruse.tech/dev" // Use the production endpoint
+    : "https://api.ruse.tech"; // Use the development endpoint or any other desired value for 'dev'
