355f014c81f23dae57eed3e83fcc3aa349010049
seb <seb@ruse.tech>
2024-07-26 16:24:11 +0000

    fix color and format

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,24 +5,16 @@ import ruse_dark from "./ruse.png";
 const About = () => {
   return (
     <div className={style.center}>
-      <img src={ruse_dark} className={style.ruseImg} alt="Ruse Logo"></img>
-      <p className={style.boxedAbout}>
-        Pentester turned Cloud Security Engineer with an interest in privacy. This website is dedicated to hosting a collection of content that I find intresting or relevant. If you wish to get in touch, please feel free to contact me via email:
-        {" "}
-        <a href="seb@ruse.tech"> seb@ruse.tech </a>
-        <p></p>
-        <ul>
-          <li>
-            <a
-              href="https://github.com/sebastian-mora"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Github
-            </a>
-          </li>
-        </ul>
-      </p>
+      <img src={ruse_dark} className={style.ruseImg} alt="Ruse Logo" />
+      <div className={style.boxedAbout}>
+        <p>Pentester turned Security Engineer with an interest in privacy. This website is dedicated to hosting a collection of content that I find interesting or relevant.</p>
+        <p>
+          <a href="mailto:ruse@ruse.tech">ruse@ruse.tech</a>
+        </p>
+        <p>
+          <a href="https://github.com/sebastian-mora" target="_blank" rel="noopener noreferrer">github.com/sebastian-mora</a>
+        </p>
+      </div>
     </div>
   );
 };
